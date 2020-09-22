4c951fc2e114d491f39ba6c50aa2b314843364de
johnny-yu <290425166@qq.com>
2020-09-22 18:10:54 +0800

    Initial commit

---
+++ search.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+
+search_url = 'https://search.itunes.apple.com/WebObjects/MZSearchHints.woa/wa/hints?clientApplication=Software&caller=com.apple.AppStore&v=4&term=vlc'
+
+search_url = 'https://amp-api-search-edge.apps.apple.com/v1/catalog/cn/search?platform=iphone&additionalPlatforms=appletv%2Cipad%2Cmac%2Cwatch&extend=editorialBadgeInfo%2CmessagesScreenshots%2CminimumOSVersion%2CrequiredCapabilities%2CscreenshotsByType%2CsupportsFunCamera%2CvideoPreviewsByType&extend%5Beditorial-items%5D=showLabelInSearch&include=apps%2Ctop-apps&include%5Beditorial-items%5D=marketing-items&meta%5Bmarketing-items%5D=metrics&limit%5Bads-result%5D=4&bubble%5Bsearch%5D=apps%2Cdevelopers%2Cgroupings%2Ceditorial-items%2Capp-bundles%2Cin-apps&term=vlc&with=spellCheck&l=zh-Hans-CN'
+
+heared = {
+	
+}
